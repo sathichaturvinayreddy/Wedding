a2e0178a19e013e02f390679ffc2dabd32a221d6
sathichaturvinayreddy <luckylucky9995@gmail.com>
2024-09-25 20:04:51 +0530

    Commit

--- js/main.js
+++ js/main.js
@@ -24,3 +24,51 @@ window.addEventListener('load',()=>{
         });
     });
 });
+
+// activate sections
+
+const sectionNavigator = name => {
+    let sections = document.querySelectorAll('section');
+    let header = document.querySelector('header');
+    sections.forEach(section =>{
+        section.classList.remove('section-show');
+        if(section.classList.contains(name)){
+            section.classList.add('section-show');
+            header.classList.add('active');
+        }
+    });
+};
+
+//navigation to sections
+window.addEventListener('load', ()=>{
+    const navList = document.querySelectorAll('.nav-btn');
+    navList.forEach(nav => {
+        nav.addEventListener('click', function(e){
+            e.preventDefault();
+            navList.forEach(el=>{
+                el.classList.remove('active');
+            });
+            this.classList.add('active');
+            sectionNavigator(this.getAttribute('data-target'));
+            screen.width<768 && toggleMenu();
+        });
+    });
+
+});
+
+//reset header to initial state
+const resetHeader =() =>{
+    let header= document.querySelector('header');
+    header.classList.remove('active');
+};
+// initial navigation
+const initNavigation =() =>{
+    const navList=document.querySelectorAll('.nav-bar');
+    navList.forEach(el =>{
+        el.classList.remove('active');
+        if(el.getAttribute('data-target') === 'about'){
+            el.classList.add('active');
+        }
+    });
+    sectionNavigator('about');
+};
